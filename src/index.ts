8930f97dacb67c7f0d19b7a25e30dd95f4a432cd
yamauchi <yamauchi@r3it.com>
2023-02-25 14:41:38 +0900

    add app.get('/ab/page'

--- src/index.ts
+++ src/index.ts
@@ -14,6 +14,16 @@ const app = new Hono()
 // 	return c.json({hoge: 'fuga'})
 // })
 
+app.get('/ab/page', (c) => {
+	console.log(c.req.url)
+	const url = new URL(c.req.url)
+	url.port = '3000'	// 8787 で受けたのを 3000 にリダイレクトする
+	url.pathname = Math.random() < 0.5 ? '/ab/page-a' : '/ab/page-b'
+	// const raw = c.req.raw
+	return fetch(url, { headers: c.req.headers, body: c.req.body })
+})
+
+
 app.all('*', (c) => {
 	console.log(c.req.url)
 	const url = new URL(c.req.url)

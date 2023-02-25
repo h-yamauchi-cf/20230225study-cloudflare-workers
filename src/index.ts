9e97923044a18e8932be07bc3081b5f1257d8159
yamauchi <yamauchi@r3it.com>
2023-02-25 14:31:55 +0900

    プロキシ

--- src/index.ts
+++ src/index.ts
@@ -2,16 +2,24 @@ import { Hono } from 'hono'
 
 const app = new Hono()
 
-app.get('/', (c) => {
-	return c.text('Hello World!')
-})
+// app.get('/', (c) => {
+// 	return c.text('Hello World!')
+// })
 
-app.get('/ping', (c) => {
-	return c.text('pong')
-})
+// app.get('/ping', (c) => {
+// 	return c.text('pong')
+// })
 
-app.get('/json', (c) => {
-	return c.json({hoge: 'fuga'})
+// app.get('/json', (c) => {
+// 	return c.json({hoge: 'fuga'})
+// })
+
+app.all('*', (c) => {
+	console.log(c.req.url)
+	const url = new URL(c.req.url)
+	url.port = '3000'	// 8787 で受けたのを 3000 にリダイレクトする
+	// const raw = c.req.raw
+	return fetch(url, { headers: c.req.headers, body: c.req.body })
 })
 
 export default app
